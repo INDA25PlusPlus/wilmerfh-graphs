8c0bad46b850334544e22554b7e7c0b5ac3b5dc0
Wilmer Fredriksson Handler <wilmerfhandler@gmail.com>
2026-02-02 15:55:35 +0100

    basic graph representation

---
+++ minimum_spanning_tree.py
@@ -0,0 +1,34 @@
+from typing import Iterator, List
+
+
+class Edge:
+    def __init__(self, a: int, b: int, w: int) -> None:
+        self.a, self.b, self.w = a, b, w
+
+
+class Graph:
+    def __init__(self, num_nodes: int, edges: List[Edge]) -> None:
+        self.num_nodes, self.edges = num_nodes, edges
+
+    @classmethod
+    def from_lines(cls, lines: Iterator[str]) -> "Graph":
+        num_nodes, num_edges = next(lines).split()
+        num_nodes, num_edges = int(num_nodes), int(num_edges)
+
+        edges = []
+        for _ in range(num_edges):
+            a, b, w = next(lines).split()
+            edges.append(Edge(int(a), int(b), int(w)))
+
+        return cls(num_nodes, edges)
+
+
+def main():
+    with open("input.txt", "r") as f:
+        graph = Graph.from_lines(f)
+
+    print(f"Number of Nodes: {graph.num_nodes}, Number of Edges: {len(graph.edges)}")
+
+
+if __name__ == "__main__":
+    main()
